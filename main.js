a39693dda97a1697c9b0caadd8eadf938657ef62
scoderr <BahriddinBoboyev997@gmail.com>
2022-11-10 11:42:44 +0500

    3.1 homework complete!

---
+++ main.js
@@ -0,0 +1,33 @@
+let ism = prompt ("Ismingiz?");
+let yosh = prompt ("Yoshingiz?");
+let manzil = prompt ("Yashash manzilingiz?");
+let qiziqish = prompt (ism +  " siz nimalarga qiziqasiz?");
+
+console.log(`
+
+
+                  🌟
+          🌤      🎄                               
+                 🎄🎄               
+                🎄🎁🎄
+               🎄🎄🎄🎄              
+              🎄🎄🎄🎄🎄
+             🎄🎁🎄🎄🎁🎄
+            🎄🎄🎄🎄🎄🎄🎄
+           🎄🎄🎄🎄🎄🎄🎄🎄       🛸
+          🎄🎄🎄🎄🎁🎄🎄🎄🎄
+         🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄
+        🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄
+       🎄🎄🎄🎁🎄🎄🎄🎄🎁🎄🎄🎄
+      🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄🎄
+              🟫🟫🟫🟫
+              🟫🟫🟫🟫
+              🟫🟫🟫🟫
+`)
+
+
+document.write("Ismingiz: " + ism + `<br>`);
+document.write("Yoshingiz: " + yosh + `<br>`);
+document.write("Manzilingiz: " + manzil + `<br>`);
+document.write("Qiziqishingiz: " + qiziqish + `<br>`);
+
